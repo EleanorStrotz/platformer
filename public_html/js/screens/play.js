d1951a5cb3dad0d12d75c5bcd3f1552bf7c5fb5d
EleanorStrotz <EleanorStrotz@gmail.com>
2014-04-29 13:40:00 -0700

    The player appears on the level change
    The player appears on the level change.

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -8,8 +8,7 @@ game.PlayScreen = me.ScreenObject.extend({
                 
                 me.levelDirector.loadLevel("level01");
                 
-                var player = new game.PlayerEntity(0, 420, {});
-                me.game.add(player, this.z);
+                this.resetPlayer();
 
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
@@ -23,5 +22,9 @@ game.PlayScreen = me.ScreenObject.extend({
 	onDestroyEvent: function() {
 		// remove the HUD from the game world
 		me.game.world.removeChild(this.HUD);
-	}
+	},
+                resetPlayer: function() {
+                        var player = me.pool.pull("player", 0, 420, {});
+                        me.game.world.addChild(player, 3);
+                }
 });
